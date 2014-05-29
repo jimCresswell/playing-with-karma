9ec2c15b4e9c729b509b03522c57ab95e6256730
Jim Cresswell <jim.cresswell@gmail.com>
2014-05-29 16:10:23 +0100

    Forget requirejs for now, just get a basic Mocha test working

--- karma.conf.js
+++ karma.conf.js
@@ -10,13 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha', 'requirejs'],
+    frameworks: ['mocha'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'test/js/test-main.js',
-      {pattern: 'test/js/*Test.js', included: false},
+      {pattern: 'test/*Test.js', included: false},
     ],
 
 
